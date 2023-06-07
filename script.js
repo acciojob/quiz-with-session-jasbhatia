2f324be58dd02b3aad5d0a8d7d413bc51b096d32
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-06-07 17:55:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-//your JS code here.
-
-// Do not change code below this line
-// This code will just display the questions to the screen
+// Quiz questions
 const questions = [
   {
     question: "What is the capital of France?",
@@ -30,32 +27,8 @@ const questions = [
   },
 ];
 
-// Display the quiz questions and choices
-function renderQuestions() {
-  for (let i = 0; i < questions.length; i++) {
-    const question = questions[i];
-    const questionElement = document.createElement("div");
-    const questionText = document.createTextNode(question.question);
-    questionElement.appendChild(questionText);
-    for (let j = 0; j < question.choices.length; j++) {
-      const choice = question.choices[j];
-      const choiceElement = document.createElement("input");
-      choiceElement.setAttribute("type", "radio");
-      choiceElement.setAttribute("name", `question-${i}`);
-      choiceElement.setAttribute("value", choice);
-      if (userAnswers[i] === choice) {
-        choiceElement.setAttribute("checked", true);
-      }
-      const choiceText = document.createTextNode(choice);
-      questionElement.appendChild(choiceElement);
-      questionElement.appendChild(choiceText);
-    }
-    questionsElement.appendChild(questionElement);
-  }
-}
-renderQuestions();
-
-		let userAnswers = sessionStorage.getItem("progress");
+// Retrieve user progress from session storage
+let userAnswers = sessionStorage.getItem("progress");
 if (userAnswers) {
   userAnswers = JSON.parse(userAnswers);
 } else {
